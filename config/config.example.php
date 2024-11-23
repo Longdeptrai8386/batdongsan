c1c34a7e34a13d55592611082c80738230185cfb
Longdeptrai8386 <longvhpn00080@gmail.com>
2024-11-23 09:34:17 +0700

    Delete  class danh muc

--- config/config.example.php
+++ config/config.example.php
@@ -3,13 +3,13 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL'       , 'http://biznews_rebuild.test/');
-define('BASE_URL_ADMIN'       , 'http://biznews_rebuild.test/admin/');
+define('BASE_URL'       , 'http://batdongsan.test/');
+define('BASE_URL_ADMIN'       , 'http:/batdongsan.test/admin/');
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME'    , 'biznews');
+define('DB_NAME'    , '<db_name>');
 define('DB_CHARSET', 'utf8');
 // đẩy đường dẫn lên cấp cao nhất
 define('PATH_ROOT'    , __DIR__ . '/../');
